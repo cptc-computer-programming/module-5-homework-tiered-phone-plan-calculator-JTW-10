f64007c4ec4875d7dbfaf1355a183d8a7cb45bcc
Jay Goulbourne <83363676+JTW-10@users.noreply.github.com>
2026-05-06 08:52:02 -0700

    Added all inputs and some variables

--- data_bill.py
+++ data_bill.py
@@ -5,8 +5,11 @@ PREMIUM_USER_OVERAGE_RATE_TIER_2 = 1
 REGULAR_USER_OVERAGE_RATE_TIER_2 = 2
 PREMIUM_USER_OVERAGE_RATE_TIER_3 = 2
 REGULAR_USER_OVERAGE_RATE_TIER_3 = 3
-
+FINAL_OVERAGE_COST = 0
 
 # Your code goes here:
-USER_DATA_USED = float(input("Please enter the data you have used (GB): "))
-print(f"You have used {USER_DATA_USED} GB.")
+user_data = float(input("Please enter the data you have used (GB): "))
+base_plan_cost = float(input("Please enter your base monthly plan cost: "))
+has_premium = input("Do you have a premium plan? (yes/no): ") == "yes"
+
+overage_data = user_data - TIER_1_DATA_LIMIT_GB
