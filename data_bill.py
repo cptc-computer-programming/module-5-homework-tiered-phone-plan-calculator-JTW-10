4868abee1bfa81f6e7be903d1c10ce34157d4589
Jay Goulbourne <83363676+JTW-10@users.noreply.github.com>
2026-05-06 09:15:57 -0700

    Added overage rate to final results
    This is to reflect the sample runs in the readme. Overage rate is added to results only if user went over the data limit.

--- data_bill.py
+++ data_bill.py
@@ -6,6 +6,7 @@ REGULAR_USER_OVERAGE_RATE_TIER_2 = 2
 PREMIUM_USER_OVERAGE_RATE_TIER_3 = 2
 REGULAR_USER_OVERAGE_RATE_TIER_3 = 3
 final_overage_cost = 0
+final_overage_rate = 0
 
 # Your code goes here:
 user_data = float(input("Please enter the data you have used (GB): "))
@@ -19,21 +20,26 @@ if user_data <= TIER_1_DATA_LIMIT_GB:
 elif user_data > TIER_1_DATA_LIMIT_GB and user_data <= TIER_2_DATA_LIMIT_GB:
     if has_premium:
         final_overage_cost = PREMIUM_USER_OVERAGE_RATE_TIER_2 * overage_data
+        final_overage_rate = PREMIUM_USER_OVERAGE_RATE_TIER_2
     else:
         final_overage_cost = REGULAR_USER_OVERAGE_RATE_TIER_2 * overage_data
+        final_overage_rate = REGULAR_USER_OVERAGE_RATE_TIER_2
 else:
     if has_premium:
         final_overage_cost = PREMIUM_USER_OVERAGE_RATE_TIER_3 * overage_data
+        final_overage_rate = PREMIUM_USER_OVERAGE_RATE_TIER_3
     else:
         final_overage_cost = REGULAR_USER_OVERAGE_RATE_TIER_3 * overage_data
+        final_overage_rate = REGULAR_USER_OVERAGE_RATE_TIER_3
 
 total_cost = base_plan_cost + final_overage_cost
 
 if user_data > 10:
     print(f"\nYou are {overage_data:.2f} GB over your data limit.")
+    print(f"Overage Rate: ${final_overage_rate:.2f} Per GB.")
 else:
     print("You are within your data limit.")
 
 print(f"Overage Cost: ${final_overage_cost:.2f}")
-print(f"Total Bill: ${total_cost}")
+print(f"Total Bill: ${total_cost:.2f}")
 
